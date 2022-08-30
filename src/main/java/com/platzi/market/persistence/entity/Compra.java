405aa6626689dd537fcf109ddea8fd7dc867708b
Brayantoro <92780108+Brayantoro@users.noreply.github.com>
2022-08-30 12:46:38 -0500

    firs

---
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -0,0 +1,34 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name ="id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name ="id_cliente",insertable = false,updatable = false)
+    private Cliente cliente;
+
+
+
+}
